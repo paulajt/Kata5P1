07a7f0fb1d596aa51d8edd9bb5b25049ab21549f
paula <paula@10.225.170.230>
2018-01-17 18:29:58 +0000

    Kata5_P1, primer commit.

---
+++ src/kata5_p1/Kata5_P1.java
@@ -0,0 +1,26 @@
+
+package kata5_p1;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class Kata5_P1 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+         Class.forName("org.sqlite.JDBC");	
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\paula\\Desktop\\Kata5.db");	
+    
+        Statement statement = (Statement) connection.createStatement();        
+        ResultSet rs = statement.executeQuery("SELECT * FROM PEOPLE;");
+                  
+        while (rs.next()) {
+        System.out.println("ID = " + rs.getInt("Id"));
+        System.out.println("NAME = " + rs.getString("Nombre"));
+        }
+    }
+    
+}
